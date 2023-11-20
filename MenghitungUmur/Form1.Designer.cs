81bde162c91ee88e3885e234ea80043e17b2e338
AndryAditama <andryaditama@gmail.com>
2023-11-20 12:06:00 +0700

    first commit

---
+++ MenghitungUmur/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MenghitungUmur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbUmurHari = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbUmurBulan = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbUmurTahun = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tbUmurHari);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbUmurBulan);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbUmurTahun);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(439, 194);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Menghitung Umur";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(382, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Hari";
+            // 
+            // tbUmurHari
+            // 
+            this.tbUmurHari.Location = new System.Drawing.Point(330, 141);
+            this.tbUmurHari.Name = "tbUmurHari";
+            this.tbUmurHari.Size = new System.Drawing.Size(46, 20);
+            this.tbUmurHari.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(290, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Bulan";
+            // 
+            // tbUmurBulan
+            // 
+            this.tbUmurBulan.Location = new System.Drawing.Point(238, 141);
+            this.tbUmurBulan.Name = "tbUmurBulan";
+            this.tbUmurBulan.Size = new System.Drawing.Size(46, 20);
+            this.tbUmurBulan.TabIndex = 10;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(144, 47);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(264, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(195, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Tahun";
+            // 
+            // tbUmurTahun
+            // 
+            this.tbUmurTahun.Location = new System.Drawing.Point(143, 141);
+            this.tbUmurTahun.Name = "tbUmurTahun";
+            this.tbUmurTahun.Size = new System.Drawing.Size(46, 20);
+            this.tbUmurTahun.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Umur";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hari Lahir";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(143, 78);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tanggal Lahir";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tanggal Sekarang";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 263);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbUmurTahun;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox tbUmurBulan;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbUmurHari;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MenghitungUmur/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MenghitungUmur
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            //menentukan hari lahir
+            var tgl = dateTimePicker2.Text.ToString();
+            String[] hari = tgl.Split(',');
+            textBox1.Text = hari[0];
+            
+            //split tanggal saat ini
+            var dtSaatIni = dateTimePicker1.Value.ToString();
+            String[] splitTglSaatini = dtSaatIni.Split('/', ' ');
+            var tglSaatIni = int.Parse(splitTglSaatini[0]);
+            var bulanSaatIni = int.Parse(splitTglSaatini[1]);
+            var tahunSaatini = int.Parse(splitTglSaatini[2]);
+
+            //split tanggal lahir
+            var dtTglLahir = dateTimePicker2.Value.ToString();
+            String[] splitTglLahir = dtTglLahir.Split('/', ' ');
+            var tglLahir = int.Parse(splitTglLahir[0]);
+            var bulanLahir = int.Parse(splitTglLahir[1]);
+            var tahunLahir = int.Parse(splitTglLahir[2]);
+
+            //menghitung umur
+            int hasilTanggal = tglSaatIni - tglLahir;
+            int hasilTanggal2 = tglLahir - tglSaatIni;
+            int hasilBulan = bulanSaatIni - bulanLahir;
+            int hasilTahun = tahunSaatini - tahunLahir;
+            int cekBulan;
+
+            if (bulanSaatIni < bulanLahir)
+            {
+                hasilTahun = hasilTahun - 1;
+                cekBulan = 12 - bulanLahir + bulanSaatIni;
+            }
+            if ((tahunSaatini > tglLahir) && (tglSaatIni >= tglLahir) && (bulanSaatIni >= bulanLahir))
+            {
+                tbUmurTahun.Text = hasilTahun.ToString();
+                tbUmurBulan.Text = hasilBulan.ToString();
+                tbUmurHari.Text = hasilTanggal.ToString();
+            }
+            else if ((tahunSaatini > tglLahir) && (tglSaatIni <= tglLahir) && (bulanSaatIni <= bulanLahir)) 
+            {
+                cekBulan = 12 - bulanLahir + bulanSaatIni;
+                tbUmurTahun.Text = hasilTahun.ToString();
+                tbUmurBulan.Text = cekBulan.ToString();
+                tbUmurHari.Text = hasilTanggal2.ToString();
+            }
+            else if ((tahunSaatini > tglLahir) && (tglSaatIni >= tglLahir) && (bulanSaatIni <= bulanLahir))
+            {
+                cekBulan = 12 - bulanLahir + bulanSaatIni;
+                tbUmurTahun.Text = hasilTahun.ToString();
+                tbUmurBulan.Text = cekBulan.ToString();
+                tbUmurHari.Text = hasilTanggal.ToString();
+            }
+            else if ((tahunSaatini > tglLahir) && (tglSaatIni <= tglLahir) && (bulanSaatIni >= bulanLahir))
+            {
+                //cekBulan = 12 - bulanLahir + bulanSaatIni;
+                tbUmurTahun.Text = hasilTahun.ToString();
+                tbUmurBulan.Text = hasilBulan.ToString();
+                tbUmurHari.Text = hasilTanggal2.ToString();
+            }
+            
+            //textBox2.Text = tglSaatIni.ToString()+ " " +bulanSaatIni.ToString()+ " " +tahunSaatini.ToString();
+            //textBox2.Text = tglLahir.ToString() + " " + bulanLahir.ToString() + " " + tahunLahir.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dateTimePicker1.CustomFormat = "dddd, dd MMMM yyyy";
+            dateTimePicker2.CustomFormat = "dddd, dd MMMM yyyy";
+        }
+        
+    }
+}
